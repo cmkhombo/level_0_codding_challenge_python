23ccab53872fe271a3d717b24046205ede13f0b1
cmkhombo <54800754+cmkhombo@users.noreply.github.com>
2022-07-27 23:58:11 +0200

    Update task05.py
    Removed import math from the code

--- task05.py
+++ task05.py
@@ -1,17 +1,9 @@
-import math
-
-
 def area_of_triangle(_first_num, _second_num, _third_num):
     _sides = (_first_num + _second_num + _third_num) / 2
 
-    _area = math.sqrt(
-        (
-            _sides
-            * (_sides - _first_num)
-            * (_sides - _second_num)
-            * (_sides - _third_num)
-        )
-    )
+    _area = (
+        _sides * (_sides - _first_num) * (_sides - _second_num) * (_sides - _third_num)
+    ) ** 0.5
 
     print(_area)
 
