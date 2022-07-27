fa3ff5a1740b8432e9568c6c84bbe63c433be9e3
cmkhombo <54800754+cmkhombo@users.noreply.github.com>
2022-07-27 23:56:43 +0200

    Update task08.py
    Added f-string to format the string and removed import math, changed strings (minutes and hours) to start with small letters

--- task08.py
+++ task08.py
@@ -1,31 +1,31 @@
-import math
-
-
 def numbers_to_time(_number):
-    _hours = math.floor(_number / 60)
 
+    _time = _number / 60
+    _hours_str = str(_time)
+    _hrs, _mins = _hours_str.split(".")
+    _hours = int(_hrs)
     _min = 0
 
     if _number > 0 and _number != 60:
         _min = _number % 60
 
     if _hours > 1 or _hours == 0:
-        _hrs_string = "{} Hours"
+        _hrs_string = f"{_hours} hours"
 
     elif _hours == 1 and _min == 0:
         _min = 60
         _hours = 0
-        _hrs_string = "{} Hours"
+        _hrs_string = f"{_hours} hours"
 
     elif _hours == 1 and _min > 0:
-        _hrs_string = "{} Hour"
+        _hrs_string = f"{_hours} hour"
 
     if _min > 1 or _min == 0:
-        _min_string = ", {} Minutes"
+        _min_string = f", {_min} minutes"
     else:
-        _min_string = ", {} Minute"
+        _min_string = f", {_min} minute"
 
-    return _hrs_string.format(_hours) + _min_string.format(_min)
+    return _hrs_string + _min_string
 
 
-print(numbers_to_time(71))
+print(numbers_to_time(133))
