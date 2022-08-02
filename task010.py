36f2c1493bb0f310e6f2ca18daa4281a7f90958a
cmkhombo <54800754+cmkhombo@users.noreply.github.com>
2022-08-02 15:18:44 +0200

    Update task010.py

--- task010.py
+++ task010.py
@@ -1,9 +1,9 @@
-def common_letters(_str_one, _str_two):
-    _str_one_lower = _str_one.lower()
-    _str_two_lower = _str_two.lower()
+def common_letters(str_one, str_two):
+    str_one_lower = str_one.lower()
+    str_two_lower = str_two.lower()
     print(
         "Common letters:",
-        ",".join(set.intersection(set(_str_one_lower), set(_str_two_lower))),
+        ",".join(set.intersection(set(str_one_lower), set(str_two_lower))),
     )
 
 
