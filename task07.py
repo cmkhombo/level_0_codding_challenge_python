f136957d1618494b72bb1417d2f55794e2895b9a
cmkhombo <54800754+cmkhombo@users.noreply.github.com>
2022-08-02 15:34:47 +0200

    Update task07.py

--- task07.py
+++ task07.py
@@ -1,11 +1,11 @@
-def celsius_to_fahrenheit(_celsius):
-    _fahrenheit = (_celsius * 9) / 5 + 32
-    return _fahrenheit
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = (celsius * 9) / 5 + 32
+    return fahrenheit
 
 
-def fahrenheit_to_celsius(_fahrenheit):
-    _celsius = (5 / 9) * (_fahrenheit - 32)
-    return _celsius
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = (5 / 9) * (fahrenheit - 32)
+    return celsius
 
 
 print(celsius_to_fahrenheit(13))
