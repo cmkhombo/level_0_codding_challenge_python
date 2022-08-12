d46fd61badbafcfd4298972ca83eed44931ae7a2
cmkhombo <54800754+cmkhombo@users.noreply.github.com>
2022-08-12 19:13:21 +0200

    Update task0_5.py

--- task0_5.py
+++ task0_5.py
@@ -5,7 +5,7 @@ def area_of_triangle(first_num, second_num, third_num):
         sides * (sides - first_num) * (sides - second_num) * (sides - third_num)
     ) ** 0.5
 
-    print(area)
+    return area
 
 
-area_of_triangle(6, 4, 3)
+print(area_of_triangle(6, 4, 3))
